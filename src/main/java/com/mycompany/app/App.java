00f6ae283c6a0a71a268c155d20e44718b159ad6
Dalipt <daliptrehan@gmail.com>
2024-08-20 13:18:53 +0530

    Update App.java
    addition check

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,6 +7,9 @@ package com.mycompany.app;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("The sum is "+c);
     }
 }
